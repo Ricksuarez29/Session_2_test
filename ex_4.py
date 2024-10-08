67472875ec15b443d3652a634cdcea2d379402d4
Ricardo Suarez <ricardoj.suarez29@gmail.com>
2024-10-08 13:43:02 +0200

    Exercise 4

---
+++ ex_4.py
@@ -0,0 +1,11 @@
+## Exercise 4: Temperature Converter
+# 1. Define a Conversion Function: Write a function celsius_to_fahrenheit(celsius) that converts Celsius to Fahrenheit.
+# 2. Print the Result: Output the converted temperature for 22ºF, 46ºF, 51ºF and 76ºF.
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+
+print(celsius_to_fahrenheit(22))
+print(celsius_to_fahrenheit(46))
+print(celsius_to_fahrenheit(51))
+print(celsius_to_fahrenheit(76))
