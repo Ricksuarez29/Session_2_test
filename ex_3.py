b020961a62de5f9a74c4246fc38ed29fe464e922
Ricardo Suarez <ricardoj.suarez29@gmail.com>
2024-10-08 13:29:23 +0200

    Exercise 3

---
+++ ex_3.py
@@ -0,0 +1,13 @@
+## Exercise 3: Simple To-Do List 
+# 1. Create an Empty List: Start with an empty list called todo_list. 
+# 2. Define Functions:
+# A function add_task(task) that adds a task to the list.
+# A function show_tasks() that prints all tasks in the list.
+
+
+todo_list = []
+def add_task(task):
+    todo_list.append(task)
+def show_tasks():
+    print (todo_list)
+
